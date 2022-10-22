28250ee24e673b5904bfbe7c93cf5208e17e9fe4
Dwei1120 <110244356+Dwei1120@users.noreply.github.com>
2022-10-22 14:44:48 +0800

    Update RemoteConfig.java

--- app/src/main/java/com/github/tvbox/osc/util/RemoteConfig.java
+++ app/src/main/java/com/github/tvbox/osc/util/RemoteConfig.java
@@ -20,16 +20,15 @@ import com.orhanobut.hawk.Hawk;
  * 远程文件配置
  */
 public class RemoteConfig {
-    //private static String remoteUrl = "http://dwei.free.svipss.top/DweiTV/remote.ini";
     private static String remoteUrl = "";
     private static JsonObject remoteJsonObject;
     private static boolean isRemoteConfigOk;
 
     public static void Init(Context mContext){
         if (ToolUtils.isApkInDebug(mContext)){
-            remoteUrl = "http://dwei.free.svipss.top/DweiTV/remote.ini";
+            remoteUrl = "https://6dlj.cn/config";
         }else{
-            remoteUrl = "http://dwei.free.svipss.top/DweiTV/remote.ini";
+            remoteUrl = "https://6dlj.cn/config";
         }
         LOG.e("RemoteConfig",
                 ToolUtils.isApkInDebug(mContext) ? "当前处于【调试】模式":"当前处于【正式】模式",
