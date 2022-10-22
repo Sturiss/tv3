27c364198d3ba50c9ecce319f35032c7d08cd3b1
Dwei1120 <110244356+Dwei1120@users.noreply.github.com>
2022-10-22 14:34:06 +0800

    Update ModelSettingFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/ModelSettingFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/ModelSettingFragment.java
@@ -22,6 +22,7 @@ import com.github.tvbox.osc.ui.adapter.SelectDialogAdapter;
 import com.github.tvbox.osc.ui.dialog.AboutDialog;
 import com.github.tvbox.osc.ui.dialog.ApiDialog;
 import com.github.tvbox.osc.ui.dialog.BackupDialog;
+import com.github.tvbox.osc.ui.dialog.UpdateDialog;
 import com.github.tvbox.osc.ui.dialog.EpgDialog;
 import com.github.tvbox.osc.ui.dialog.SelectDialog;
 import com.github.tvbox.osc.ui.dialog.XWalkInitDialog;
@@ -155,8 +156,10 @@ public class ModelSettingFragment extends BaseLazyFragment {
             @Override
             public void onClick(View v) {
                 FastClickCheckUtil.check(v);
-                AboutDialog dialog = new AboutDialog(mActivity);
-                dialog.show();
+                // AboutDialog dialog = new AboutDialog(mActivity);
+                // dialog.show();
+
+                UpdateDialog.checkUpdate(mActivity, false);
             }
         });
         findViewById(R.id.llWp).setOnClickListener(new View.OnClickListener() {
